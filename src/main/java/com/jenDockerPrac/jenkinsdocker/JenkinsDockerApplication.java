022f2bcd1dfc7bed7d2ff80307dd21cdd17dfc19
udayprakashmishra <udayprakashmishra@hotmail.com>
2021-05-31 15:45:11 +0530

    Add files via upload

---
+++ src/main/java/com/jenDockerPrac/jenkinsdocker/JenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.jenDockerPrac.jenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerApplication.class, args);
+	}
+
+}
